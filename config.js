e502a9302adfadb13d1a6d252963e51ca5d82e38
Luvvydev <43109603+Luvvydev@users.noreply.github.com>
2026-08-25 23:18:11 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+window.MEMBER_DOWNLOAD_CONFIG = {
+  productName: "Member Download",
+  priceLabel: "$20",
+  downloadReady: false,
+  firebase: {
+    apiKey: "AIzaSyCaPio0QutVP3MX13fUge4Lo9_SVkIcSMQ",
+    authDomain: "chess-ee6b0.firebaseapp.com",
+    projectId: "chess-ee6b0",
+    storageBucket: "chess-ee6b0.firebasestorage.app",
+    messagingSenderId: "1041277387280",
+    appId: "1:1041277387280:web:b65a708c184d4263640095",
+    measurementId: "G-Z6H38DTWPY",
+  },
+  appCheckSiteKey: "",
+};
