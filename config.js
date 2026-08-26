384085f255d7be09f0292fefac4152819c1ecf2e
Luvvydev <43109603+Luvvydev@users.noreply.github.com>
2026-08-25 23:36:59 -0400

    Configure Firebase App Check

--- config.js
+++ config.js
@@ -11,5 +11,5 @@ window.MEMBER_DOWNLOAD_CONFIG = {
     appId: "1:1041277387280:web:b65a708c184d4263640095",
     measurementId: "G-Z6H38DTWPY",
   },
-  appCheckSiteKey: "",
+  appCheckSiteKey: "6LfRCJktAAAAANLq1sbPcKCwTAHmuBhamScGuA6Z",
 };
